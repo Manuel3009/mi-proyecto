d772d6a5297e5ec77b6325fcc225050eefc6c97c
Manuel Tabarez <manuel.tabarez@alumnos.udg.mx>
2016-12-06 18:20:10 -0600

    agregada la clase sonido

--- archivo1.py
+++ archivo1.py
@@ -6,4 +6,12 @@ class Imagen():
         pass
     def binarizacion(self):
         pass
+
+class Sonido():
+    def reproducir(self):
+        pass
+    def volumen(self):
+        pass
+    def fourier(self):
+        pass
     
